6e137d485b755337d64558a768079dca2175f1ed
rs-vinhdt <vinhdt@runsystem.net>
2021-08-27 00:55:17 +0700

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppFooterComponent } from './shared/app-footer/app-footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TopMenuComponent } from './shared/top-menu/top-menu.component';
+import { LeftMenuComponent } from './shared/left-menu/left-menu.component';
+import { DashboardFooterComponent } from './shared/dashboard-footer/dashboard-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppFooterComponent,
+    LoginComponent,
+    DashboardComponent,
+    TopMenuComponent,
+    LeftMenuComponent,
+    DashboardFooterComponent
   ],
   imports: [
     BrowserModule,
